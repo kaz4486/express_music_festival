6e88d54803f6bf6f0bca027f9d75e43cc624ed79
Kamil <kaz4486@gmail.com>
2022-09-06 21:22:53 +0200

    Modify config.js

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,4 @@ export const API_URL =
   process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000/api';
 
 export const CLIENT_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'http://localhost:3000'
-    : 'http://localhost:8000';
+  process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:8000';
