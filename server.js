ef430e7805a9a8235dd05fe03994685a7828a667
Kamil <kaz4486@gmail.com>
2022-09-06 20:33:25 +0200

    Modify io in server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
-const io = socket(server, { cors: { origin: '*' } });
+const io = socket(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
 
 io.on('connection', (socket) => {
   console.log('New socket!' + socket.id);
