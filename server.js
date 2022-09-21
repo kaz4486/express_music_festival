012fdd51b7c51300de30763a59b4171e5654e86b
Kamil <kaz4486@gmail.com>
2022-09-21 11:33:38 +0200

    Add heroku config to connection string

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ app.use((req, res) => {
 });
 
 const NODE_ENV = process.env.NODE_ENV;
+const MongoDB_Username = process.env.MONGODB_USERNAME;
+const MongoDB_Password = process.env.MONGODB_PASSWORD;
 let dbUri = '';
 
 if (NODE_ENV === 'production') dbUri = 'url to remote db';
@@ -57,13 +59,16 @@ else if (NODE_ENV === 'test')
   dbUri = 'mongodb://localhost:27017/NewWaveDBDBtest';
 else dbUri = 'mongodb://localhost:27017/NewWaveDB';
 
-mongoose.connect(
-  'mongodb+srv://kaz4486:Robinho789@cluster0.telw8lc.mongodb.net/NewWaveDB?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+if (NODE_ENV === 'production')
+  connectionString = `mongodb+srv://${MongoDB_Username}:${MongoDB_Password}@cluster0.telw8lc.mongodb.net/NewWaveDB?retryWrites=true&w=majority`;
+else
+  connectionString =
+    'mongodb+srv://kaz4486:Robinho789@cluster0.telw8lc.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
 db.once('open', () => {
